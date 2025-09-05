eeb43661e7254159cff778f3421a1d88d8fc91a7
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-09-05 14:41:31 +0000

    SDK regeneration

--- src/api/resources/documents/client/requests/index.ts
+++ src/api/resources/documents/client/requests/index.ts
@@ -1,1 +1,3 @@
+export { type PostInteractionsIdDocumentsRequest } from "./PostInteractionsIdDocumentsRequest.js";
+export { type GetInteractionsIdDocumentsDocumentIdRequest } from "./GetInteractionsIdDocumentsDocumentIdRequest.js";
 export { type DocumentsUpdateRequest } from "./DocumentsUpdateRequest.js";
