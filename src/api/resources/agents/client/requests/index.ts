2eba8a4205a1cbdcdce6ba4cb51c5184892a2b21
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-09-26 13:26:37 +0000

    SDK regeneration

---
+++ src/api/resources/agents/client/requests/index.ts
@@ -0,0 +1,4 @@
+export { type AgentsListRequest } from "./AgentsListRequest.js";
+export { type AgentsCreateAgent } from "./AgentsCreateAgent.js";
+export { type AgentsGetTaskRequest } from "./AgentsGetTaskRequest.js";
+export { type AgentsGetContextRequest } from "./AgentsGetContextRequest.js";
