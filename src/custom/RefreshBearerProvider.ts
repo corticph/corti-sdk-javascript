ada2aaf1d6b8498ff9170c4acc1341af37fc0588
markitosha <markitosha@gmail.com>
2025-11-03 11:23:12 +0100

    feat: parse token expiry when passed and update refresh token handling

--- src/custom/RefreshBearerProvider.ts
+++ src/custom/RefreshBearerProvider.ts
@@ -4,6 +4,7 @@
 
 import * as core from "../core/index.js";
 import * as api from "../api/index.js";
+import { decodeToken } from "./utils/decodeToken.js";
 
 type ExpectedTokenResponse = Omit<api.GetTokenResponse, 'tokenType'> & { tokenType?: string };
 type RefreshAccessTokenFunction = (refreshToken?: string) => Promise<ExpectedTokenResponse> | ExpectedTokenResponse;
@@ -14,10 +15,7 @@ export type BearerOptions = Partial<Omit<api.GetTokenResponse, 'accessToken'>> &
 } | {
     refreshAccessToken: RefreshAccessTokenFunction;
     accessToken?: string;
-}) & {
-    expiresAt?: number;
-    refreshExpiresAt?: number;
-};
+});
 
 export class RefreshBearerProvider {
     private readonly BUFFER_IN_MINUTES = 2;
@@ -36,14 +34,17 @@ export class RefreshBearerProvider {
         refreshToken,
         refreshExpiresIn,
         expiresIn,
-        expiresAt,
-        refreshExpiresAt
     }: BearerOptions) {
-        this._expiresAt = this.getExpiresAt(expiresIn, this.BUFFER_IN_MINUTES);
-        this._refreshExpiresAt = this.getExpiresAt(refreshExpiresIn, 0);
-
         this._accessToken = accessToken || 'no_token';
         this._refreshToken = refreshToken;
+
+        this._expiresAt = typeof expiresIn === "number"
+            ? this.getExpiresAt(expiresIn, this.BUFFER_IN_MINUTES)
+            : this.parseTokenExpiry(this._accessToken, this.BUFFER_IN_MINUTES) || this.getExpiresAt(0, this.BUFFER_IN_MINUTES);
+
+        this._refreshExpiresAt = typeof refreshExpiresIn === "number"
+            ? this.getExpiresAt(refreshExpiresIn, 0)
+            : (this._refreshToken && this.parseTokenExpiry(this._refreshToken, 0)) || this.getExpiresAt(0, 0);
 
         this._refreshAccessToken = refreshAccessToken;
     }
@@ -64,10 +65,14 @@ export class RefreshBearerProvider {
         const tokenResponse = await this._refreshAccessToken(this._refreshToken);
 
         this._accessToken = tokenResponse.accessToken;
-        this._expiresAt = this.getExpiresAt(tokenResponse.expiresIn, this.BUFFER_IN_MINUTES);
+        this._expiresAt = typeof tokenResponse.expiresIn === "number"
+            ? this.getExpiresAt(tokenResponse.expiresIn, this.BUFFER_IN_MINUTES)
+            : this.parseTokenExpiry(tokenResponse.accessToken, this.BUFFER_IN_MINUTES) || this.getExpiresAt(0, this.BUFFER_IN_MINUTES);
 
         this._refreshToken = tokenResponse.refreshToken;
-        this._refreshExpiresAt = this.getExpiresAt(tokenResponse.refreshExpiresIn, 0);
+        this._refreshExpiresAt = typeof tokenResponse.refreshExpiresIn === "number"
+            ? this.getExpiresAt(tokenResponse.refreshExpiresIn, 0)
+            : this.parseTokenExpiry(this._refreshToken, 0) || this.getExpiresAt(0, 0);
 
         return this._accessToken;
     }
@@ -76,5 +81,21 @@ export class RefreshBearerProvider {
         const now = new Date();
 
         return new Date(now.getTime() + expiresInSeconds * 1000 - bufferInMinutes * 60 * 1000);
+    }
+
+    private parseTokenExpiry(token: string | undefined, bufferInMinutes: number): Date | undefined {
+        if (!token || token === 'no_token') {
+            return;
+        }
+
+        try {
+            const decoded = decodeToken(token);
+
+            if (decoded && typeof decoded.expiresAt === 'number') {
+                const ms = decoded.expiresAt * 1000 - bufferInMinutes * 60 * 1000;
+
+                return new Date(ms);
+            }
+        } catch {}
     }
 }
